a6675d7652d90c7302a83aa1f1ec00c1e874046f
Rob Osterburg <robert.osterburg@gmail.com>
2023-03-18 15:51:33 -0600

    Ex 1.28 reading gzipped data files

---
+++ Work/pcost_gzip.py
@@ -0,0 +1,17 @@
+# pcost_gzip.py
+#
+# Exercise 1.27 - Read portfolioi gzipped data file and total up cost of purchasing all shares
+
+import gzip
+
+total_cost = 0
+
+with gzip.open("Data/portfolio.csv.gz", "rt") as f:
+    headers = f.readline().split(",")
+    print(headers)
+    for line in f.readlines():
+        stock, shares, price = line.split(",")
+        print(stock, shares, price.strip())
+        total_cost += float(shares) * float(price.strip())
+
+print(f"Total cost ${total_cost:,.2f}")
