c860c547bd10971cad6b365c8e3f039d33f4c9b8
Rob Osterburg <robert.osterburg@gmail.com>
2023-03-18 15:46:40 -0600

    Ex 1.27 - Reads data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.27 - Read portfolioi data file and total up cost of purchasing all shares
+
+total_cost = 0
+
+with open("Data/portfolio.csv", "rt") as f:
+    headers = f.readline().split(",")
+    print(headers)
+    for line in f.readlines():
+        stock, shares, price = line.split(",")
+        print(stock, shares, price.strip())
+        total_cost += float(shares) * float(price.strip())
+
+print(f"Total cost ${total_cost:,.2f}")
