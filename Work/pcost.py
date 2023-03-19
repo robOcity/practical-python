dc8d46a853ee4dee70a4772a09e991f2e7ebdadb
Rob Osterburg <robert.osterburg@gmail.com>
2023-03-19 14:04:03 -0600

    ex 1.32 use csv module to process data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,22 +2,43 @@
 #
 # Exercise 1.27 - Read portfolioi data file and total up cost of purchasing all shares
 
+import csv
 
-def portfolio_cost(file):
+
+def portfolio_cost_csv(filename):
     "returns the total cost to purchase the portfolio contained in file"
     total_cost = 0
-    with open(file, "rt") as f:
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            sym, shares, price = row[0], int(row[1]), float(row[2])
+            total_cost += calc_total_cost(shares, price)
+    return total_cost
+
+
+def portfolio_cost(filename):
+    "returns the total cost to purchase the portfolio contained in file"
+    total_cost = 0
+    with open(filename, "rt") as f:
         headers = f.readline().split(",")
         print(headers)
         for line in f.readlines():
-            stock, shares, price = line.split(",")
-            print(stock, shares, price.strip())
-            try:
-                total_cost += int(shares) * float(price.strip())
-            except ValueError:
-                print("Cannot parse line: ", line)
+            sym, shares, price = line.split(",")
+            total_cost += calc_total_cost(shares, price)
     return total_cost
+
+
+def calc_total_cost(shares, price):
+    "calculates the total cost of purchasing shares"
+    try:
+        return int(shares) * float(price)
+    except ValueError:
+        print(f"Cannot parse either {shares} and/or {price}")
 
 
 cost = portfolio_cost("Data/portfolio.csv")
 print(f"Total cost ${cost:,.2f}")
+
+cost = portfolio_cost_csv("Data/portfolio.csv")
+print(f"Total cost by csv ${cost:,.2f}")
