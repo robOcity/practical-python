1d75e88b881a34d4f3f84723a98f34b8773f18a0
Rob Osterburg <robert.osterburg@gmail.com>
2023-03-19 10:03:37 -0600

    adding doc string to function

--- Work/pcost.py
+++ Work/pcost.py
@@ -4,6 +4,7 @@
 
 
 def portfolio_cost(file):
+    "returns the total cost to purchase the portfolio contained in file"
     total_cost = 0
     with open(file, "rt") as f:
         headers = f.readline().split(",")
