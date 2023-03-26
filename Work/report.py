2f7113a00d6192d35f9e3e9a670cdc0ae47b1c04
Rob Osterburg <robert.osterburg@gmail.com>
2023-03-26 11:59:52 -0600

    find change in portfolio value

--- Work/report.py
+++ Work/report.py
@@ -6,7 +6,7 @@ import csv
 from pprint import pprint
 
 
-def read_portfolio(filename):
+def get_portfolio(filename):
     """Reads file provide and returns a list of dicts of stock holdings {name, shares, price}."""
     portfolio = []
 
@@ -20,10 +20,32 @@ def read_portfolio(filename):
     return portfolio
 
 
-portfolio = read_portfolio("Data/portfolio.csv")
+def get_prices(filename):
+    """Reads price data from a file and returns a dictionary {stock, price}."""
+    prices = {}
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                print(f'Unable to read "{row}"')
+    return prices
+
+
+portfolio = get_portfolio("Data/portfolio.csv")
 total_cost = 0
 for stock in portfolio:
     total_cost += stock.get("shares", 0) * stock.get("price", 0.0)
 
 pprint(portfolio)
 print(f"Total cost: ${total_cost:,.2f}")
+
+prices = get_prices("Data/prices.csv")
+change = 0.0
+for stock in portfolio:
+    cost = stock.get("shares") * stock.get("price")
+    current_value = stock.get("shares", 0) * prices.get(stock.get("name"), 0.0)
+    change += current_value - cost
+print(f"The value of the portfolio has changed by ${change:,.2f}")
