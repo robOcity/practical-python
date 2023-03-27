7ac6dda35607c893d51aebecc122f65b445aca5d
Rob Osterburg <robert.osterburg@gmail.com>
2023-03-27 12:41:35 -0600

    ex 2.16  allow records to contain more fields

--- Work/pcost.py
+++ Work/pcost.py
@@ -6,35 +6,21 @@ import csv
 import sys
 
 
-def portfolio_cost_csv(filename):
-    "returns the total cost to purchase the portfolio contained in file"
-    total_cost = 0
-    with open(filename, "rt") as f:
-        rows = csv.reader(f)
-        next(rows)  # step over header
-        for row_no, row in enumerate(rows):
-            try:
-                sym, shares, price = row[0], int(row[1]), float(row[2])
-                total_cost += int(shares) * float(price)
-            except ValueError:
-                print(f"Row #{row_no} contains missing/invalid data: {row}")
-    return total_cost
-
-
 def portfolio_cost(filename):
     "returns the total cost to purchase the portfolio contained in file"
     total_cost = 0
     with open(filename, "rt") as f:
-        headers = f.readline().split(",")
-        for line in f.readlines():
-            sym, shares, price = line.split(",")
+        rows = csv.reader(f)
+        header = next(rows)
+        print(header)
+        for row_no, row in enumerate(rows):
+            record = dict(zip(header, row))
             try:
-                total_cost += int(shares) * float(price)
+                shares = int(record["shares"])
+                price = float(record["price"])
+                total_cost += shares * price
             except ValueError:
-                print(f"Cannot parse '{shares}' or '{price}'")
-            except TypeError:
-                print(f"Cannot multiple '{shares}' and '{float(price)}'")
-
+                print(f"Row #{row_no} contains missing/invalid data: {row}")
     return total_cost
 
 
@@ -45,6 +31,3 @@ else:
 
 cost = portfolio_cost(filename)
 print(f"Total cost: ${cost:,.2f}")
-
-cost = portfolio_cost_csv(filename)
-print(f"Total cost using CSV: ${cost:,.2f}")
