44810576beae3a95f7d1ee22ecd4d6ccfadf3418
Rob Osterburg <robert.osterburg@gmail.com>
2023-03-29 11:56:43 -0600

    ex 3.4 reading csv with column selector

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,24 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+from typing import List, Dict
+
+
+def parse_csv(
+    filename: str, select: List[str] = ["price", "name", "shares"]
+) -> List[Dict[str, str]]:
+    """
+    Parses a CSV file into a list of records each represented as a dictionary.
+    """
+    records = []
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        records = [
+            {name: value for name, value in zip(header, row) if name in select}
+            for row in rows
+            if row
+        ]
+    return records
