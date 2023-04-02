f944386f6664457f421f09b68b98e7f0ea453a90
Rob Osterburg <robert.osterburg@gmail.com>
2023-04-02 13:29:51 -0600

    ex 6.6 use generator to output stock data

--- Work/follow.py
+++ Work/follow.py
@@ -3,17 +3,18 @@
 import os
 import time
 
-f = open("Data/stocklog.csv")
-f.seek(0, os.SEEK_END)
 
-while True:
-    line = f.readline()
-    if not line:
-        time.sleep(0.1)
-        continue
-    fields = line.split(",")
-    name = fields[0].strip('"')
-    price = float(fields[1])
-    change = float(fields[4])
-    if change < 0:
-        print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
+def follow(filename):
+    with open(filename, "r") as f:
+        for line in f:
+            yield line
+
+
+if __name__ == "__main__":
+    for line in follow("Data/stocklog.csv"):
+        fields = line.split(",")
+        name = fields[0].strip('"')
+        price = float(fields[1])
+        change = float(fields[4])
+        if change < 0:
+            print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
