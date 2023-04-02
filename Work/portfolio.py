63112e6e1220a7dde87d47b57fe2771f015bb440
Rob Osterburg <robert.osterburg@gmail.com>
2023-04-02 13:48:11 -0600

    ex use generator function to monitor stock prices

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -10,6 +10,15 @@ class Portfolio:
     def __iter__(self):
         return self.stocks.__iter__()
 
+    def __len__(self):
+        return len(self.stocks)
+
+    def __getitem__(self, index):
+        return self.stocks[index]
+
+    def __contains__(self, name):
+        return any([stock.name == name for stock in self.stocks])
+
     @property
     def total_cost(self):
         return sum([stock.cost for stock in self.stocks])
